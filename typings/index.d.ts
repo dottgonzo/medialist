89170a8366fde8d051f7c747b3bff199524f784b
dario <mail@dariocaruso.info>
2017-11-26 16:52:12 +0100

    remove gulp and add more entry on npmignore

---
+++ typings/index.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="globals/async/index.d.ts" />
+/// <reference path="globals/chai/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/bluebird/index.d.ts" />
+/// <reference path="modules/node-dir/index.d.ts" />
